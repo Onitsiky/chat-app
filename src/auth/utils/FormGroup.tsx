6570995b1019651314e895fd77ac69b50419a622
onitsiky <hei.onitsiky@gmail.com>
2023-05-11 08:06:49 +0300

    feat: implement signing page

---
+++ src/auth/utils/FormGroup.tsx
@@ -0,0 +1,17 @@
+import {DetailedHTMLProps, InputHTMLAttributes} from "react";
+import {UseFormRegister, FieldValues} from "react-hook-form";
+
+interface IFormGroup extends  DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>{
+    label: string,
+    inputType: string,
+    register: UseFormRegister<FieldValues>
+}
+export function FormGroup(props: IFormGroup) {
+    const {label, inputType, register} = props;
+    return(
+        <div className="form-group-container">
+            <label className="fg-label">{label}:</label>
+            <input type={inputType} className="fg-input" {...register(label)}/>
+        </div>
+    )
+}
