cd9e913cf020d6c40644051c3d1449e6a8d97c6d
onitsiky <hei.onitsiky@gmail.com>
2023-06-05 20:35:52 +0300

    feat: store current channel

--- src/components/Channel.tsx
+++ src/components/Channel.tsx
@@ -1,13 +1,23 @@
-type IChannel = {
-    initial: string,
-    name: string,
+import { useCurrentChannel } from '@/state/store'
+import { IChannel } from '@/provider/Types'
+
+type IChannelComponent = {
+    channel: IChannel,
     key: any
 }
-export function Channel({initial, name, key}: IChannel) {
-    return(
-        <div key={key} className='flex flex-row mt-10'>
-            <div className='ml-4 text-lg bg-white pr-3 pl-1 my-auto rounded text-center'>
-                {name} #
+
+export function Channel({ channel, key }: IChannelComponent) {
+    const updateCurrentChannel = useCurrentChannel(state => state.setCurrentChannel)
+
+    function handleUpdateCurrentChannel(channel: IChannel) {
+        updateCurrentChannel(channel)
+        console.log(`Current Channel is ${channel.name} with id ${channel.id}`)
+    }
+
+    return (
+        <div key={key} className='flex flex-row mt-10 cursor-pointer' onClick={() => handleUpdateCurrentChannel(channel)}>
+            <div className='ml-4 text-lg bg-white pr-3 pl-1 my-auto rounded text-center hover:bg-blue-700'>
+                {channel.name} #
             </div>
         </div>
     )
